80affc79fdd6d4ccb276998d5ed026d2b6139360
DayPad7 <dayanapadilla7@gmail.com>
2023-08-28 06:47:35 -0600

    fixed popup bg

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -7,7 +7,7 @@ function Popup(props) {
 
 
   return (props.trigger) ? (
-    <div className ='fixed flex flex-col items-center justify-center w-full h-3/6 bg-black-rgba'>
+    <div className ='fixed flex flex-col items-center justify-center w-full h-full bg-black-rgba'>
       <div className= 'absolute flex flex-col items-center justify-center bg-red-50  w-4/5 h-3/5 '>
         {props.children}
        </div>
